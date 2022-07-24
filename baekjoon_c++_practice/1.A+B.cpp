4a17a893f3b6f88914afe13cd4eee8e6dda1363e
Junhyung <amailemon98@naver.com>
2022-07-24 21:39:13 +0900

    초기 커밋.

---
+++ baekjoon_c++_practice/1.A+B.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main() {
+	int a;
+	int b;
+	int sum;
+
+	std::cout << "A와 B를 입력하세요.(0 < A, B < 10)" << std::endl;
+	std::cout << "A값: ";
+	std::cin >> a;
+	std::cout << "B값: ";
+	std::cin >> b;
+	sum = a + b;
+	std::cout << sum;
+
+}
